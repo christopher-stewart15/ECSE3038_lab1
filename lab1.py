9ba944e5ed7db2337392b75028f4b407f80939be
christopher-stewart15 <christopher_stewart15@yahoo.com>
2021-01-29 22:24:50 -0500

    Second Question

--- lab1.py
+++ lab1.py
@@ -1,4 +1,20 @@
 def hello ():
     print("ECSE3038 - Engineering Iot Systems")
 
+
+def validatePassword(passwd):
+     alnum=0
+     nums = 0
+
+    if len(passwd) > 7 and password.isalnum():
+        for val in psswd:
+            try:
+                if isinstance(int(val), int):
+                    nums += 1
+            except:
+                pass
+        
+        if nums > 1:
+            return True
     
+    return False
