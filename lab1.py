dd40fe7697a994678acfcfbaf38f560681f06c6e
christopher-stewart15 <christopher_stewart15@yahoo.com>
2021-01-29 22:36:33 -0500

    Third Question

--- lab1.py
+++ lab1.py
@@ -1,15 +1,14 @@
 def hello ():
     print("ECSE3038 - Engineering Iot Systems")
 
-
 def validatePassword(passwd):
      alnum=0
      nums = 0
 
     if len(passwd) > 7 and password.isalnum():
-        for val in psswd:
+        for dig in psswd:
             try:
-                if isinstance(int(val), int):
+                if isinstance(int(dig), int):
                     nums += 1
             except:
                 pass
@@ -18,3 +17,13 @@ def validatePassword(passwd):
             return True
     
     return False
+def sumUpToN(num):
+    dig = 0
+
+    if num > 1:
+        for i in range(1, num+1):
+            dig += i
+    else:
+        dig = -1
+
+    return dig 
